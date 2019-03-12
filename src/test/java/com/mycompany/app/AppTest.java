c30dac92dc4bb12bd670a9ce7d131e1f93f25f86
Zachary Smith <zachary.smith1@ibm.com>
2019-03-12 10:21:57 -0500

    Stuff

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,16 +2,17 @@ package com.mycompany.app;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
+
 import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
@@ -30,14 +31,21 @@ public class AppTest
     }
 
     @Test
-    public void testAppMain()
-    {
+    public void testAppMain() {
         App.main(null);
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
+    }
+
+    @Test
+    public void testStuff() {
+        int one = 2;
+        int two = 4;
+        int stuff = one + two;
+        System.out.print(stuff);
     }
 
     @After
